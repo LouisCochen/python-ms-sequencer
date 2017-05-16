5a532a773c21d984b3a6488358ed9f107038b29d
Louis Cochen <louis.cochen@live.fr>
2017-05-16 23:53:50 +0100

    Big Changes, new dict and lists to allow going back to previous branches much work needed still

--- sequencer.py
+++ sequencer.py
@@ -6,23 +6,38 @@
 # Lines with #dev are just for troubleshooting
 #
 # #### CAPITAL LETTERS : part title
-# #CAPITAL : project
+# #CAPITAL : project/idea/read the first word
 #
 # I've tried to make the variables as simple as possible.
 # The idea is reading through the peaks for the next amino acid by
 # mass of peptide + m/z of next AA. Reads the multiple possible peaks and stores
-# in multi_peaks, and the AAs in multi_AA. Then pick the first one and sequence.
+# in readings, and the AAs in multi_AA. Then pick the first one and sequence.
 # If at the end, mass!=mol, try again with second peak of the last multi found.
 # If last multi doesn't give anything go to previous and so the same.
 #
 # Exemple :
 # Find R/K as Y1 ion
 # 147,204,218,234,261,275,289,291,305
-# $K===================!                 BAD    mass!=mol
-# $K===========================!         BAD    mass!=mol
-# $K==================================$  GOOD   mass==mol
+# $K===================!                 BAD    mass_y!=mol
+# $K===========================!         BAD    mass_y!=mol
+# $K==================================$  GOOD   mass_y==mol
 
 from timeit import default_timer as timer
+
+def finding_ions(peaks_list,seq,mass_list,key,mass_change):
+    masses_dict[key]=[]
+#    print(key,'    :',masses_dict[key])
+    for n in range(len(seq)):
+        masses_dict[key].append(0)
+        if mass_list[n]+mass_change in peaks_list:
+            masses_dict[key][n]=mass_list[n]+mass_change
+            pass
+        if  masses_dict[key][n]!='' and masses_dict[key][n] in peaks_list:
+            peaks_list.remove(masses_dict[key][n])
+            pass
+        pass
+    pass
+#    print(key,'new :',masses_dict[key])
 
 MW_AA = {57:'G',71:'A',87:'S',97:'P',99:'V',101:'T',103:'C',
          113:'I/L',114:'N', 115:'D',128:'K/Q',129:'E',131:'M',137:'H',
@@ -33,45 +48,47 @@ AA_MW = {'G':57,'A':71,'S':87,'P':97,'V':99,'T':101,'C':103,
 
 ####    INPUT PEAKS
 
-# PROJECT : OCR with Tesseract from Google and pyOCR to bind it.
+# PROJECT : OCR with Tesseract from Google and pyOCR/pytesser to bind it.
 # I found : https://pythontips.com/2016/02/25/ocr-on-pdf-files-using-python/
 
 peaks_list=[]
 while peaks_list==[]:
-    in_mode=input('Input type : .txt file (F) or manual (M)?')
-# file input
+    in_mode=input('Input type : .txt file (F) or manual (M) ? ')
+#file input
     if in_mode=='F':
-        filename=input('What is the name of your file ? ')#get name of file
+        filename=input('\nWhat is the name of your file ? ')#get name of file
+        mol=int(input('\nWrite the m/z of the molecular ion :\n'))#molecular ion
+        start = timer()
         filename=filename+'.txt'
         with open(filename, "rt", encoding='utf-8') as peaks_file:#open file copy text
             for line in peaks_file:
                 for peak in line:
                     peaks_list.append(peak)
+                    pass
+                pass
         peaks_file.close()
     elif in_mode=='M':
 #manual input
-        peaks_input = input('Write your MS/MS peaks values separated by a space :\n')
+        peaks_input = input('\nWrite your MS/MS peaks values separated by a space :\n')
+        mol=int(input('\nWrite the m/z of the molecular ion :\n'))#molecular ion
+        start = timer()
         peaks_list = list(map(int, peaks_input.split(' ')))
-    elif in_mode!=='F' and in_mode=='M':
-        print('Please enter F for .txt file or M for manual.')
+        print(peaks_list)
+    elif in_mode!='F' and in_mode!='M':
+        print('Please enter F for .txt file or M for manual.\n')
     pass
 
-# List of peaks useful for troubleshooting, first list is made of actual results,
-# second list needs update to work with the new verif step.
+# List of peaks useful for troubleshooting, obtained from actual MS/MS data,
+# 704,311,125,890 are added to create a branch
+# The sequence is :['K', 'I/L', 'I/L', 'E', 'V', 'I/L', 'A', 'T', 'K/Q']
+# The branch is :                                'C', 'W'
 
 #peaks_list=[101, 129, 147, 202, 230, 260, 273, 301, 342, 373, 386, 414,
 #            485,495, 502, 513, 601, 624, 642, 656, 714, 727, 755, 785,
-#            801, 840, 868, 886, 1014]
+#            801, 840, 868, 886, 1014,704,311,125,890]
 #mol=1014
 
-#peaks_list=[147,204,218,234,261,275,289,291,305]
-#mol=305
-
-####    INPUT MOLECULAR ION M/Z
-mol=int(input('\nWrite the m/z of the molecular ion :\n'))
-
 ####    START
-start = timer()
 if mol not in peaks_list:
     peaks_list.append(mol)
 peaks_list.append(1)#for the H+ on the first b ion
@@ -83,90 +100,152 @@ print ('\nYour values are : ',', '.join(map(str,peaks_list)),
        '.\n\nm/z(molecular ion) = ',mol,'\n')
 
 ####    INITIATION OF SEQUENCING Y1 = K or R,
+masses_dict={}
 mol_b=mol-18 #mol-18 for calculating b ions
 if 147 in peaks_list and (mol_b-128) in peaks_list:
-    print('Y1=K')#dev
+    #print('Y1=K')#dev
     seq=['K']
     mass_y=147
-    mass_y_list=[mass_y]
+    masses_dict['y']=[mass_y]
+    peaks_list.remove(mass_y)
     mass_b=mol_b-128
-    mass_b_list=[mass_b]
+    masses_dict['b']=[mass_b]
+    peaks_list.remove(mass_b)
 elif 175 in peaks_list and (mol_b-156) in peaks_list:
-    print('Y1=R')#dev
+    #print('Y1=R')#dev
     seq=['R']
-    mass_y=175
-    mass_y_list=[mass_y]
-    mass_b=mol_b-128
-    mass_b_list=[mass_b]
+    mass_y=156
+    masses_dict['y']=[mass_y]
+    peaks_list.remove(mass_y)
+    mass_b=mol_b-156
+    masses_dict['b']=[mass_b]
+    peaks_list.remove(mass_b)
 else: print ('restart1')#dev
 
 ####    SEQUENCING
-list_multi=[]
+readings={}
+#format of readings :
+#readings={mass_y:[[mass_y,mass_b,seq],[y],[b],[aa]]}
+multiple=[[],[]]
+#multiple=[[mass_y before branch],[number of branches]]
 while mass_y!=mol and mass_b!=1:
-    print('mass_y :',mass_y,'mass_b :',mass_b)#dev
-    multi_peaks_y=[]
-    multi_peaks_b=[]
-    multi_AA=[]
-    print(len(multi_AA))
+    #print('SEQ while :',seq)
+    readings[mass_y]=[[mass_y,mass_b],[],[],[]]
+    #print('mass_y :',mass_y,'mass_b :',mass_b)#dev
     for x in sorted(MW_AA.keys()):
-        #print('mass+x :',mass+x,'// x :',x)#dev
+        #print('mass_y+x :',mass_y+x,'mass_b :',mass_b-x,'// x :',x)#dev
         if mass_y+x in peaks_list and mass_b-x in peaks_list:
-            #print()
-            multi_peaks_y.append(mass_y+x)
-            multi_AA.append(MW_AA[x])
-            multi_peaks_b.append(mass_b-x)
-            #safety=0
-        else:
-            #safety=safety + 1
-            continue
-        continue
-    print('peaks_y :',multi_peaks_y, '\npeaks_b :',multi_peaks_b,'\nAAs   :',multi_AA)#dev
-    list_multi.append((mass_y, list(multi_peaks_y), list(multi_peaks_b), list(multi_AA)))
-    if len(multi_peaks_y)==0 and len(multi_peaks_b)==0:
-        print('failed')#dev
-        break   #Get the next peak from the last multi.
-                #i.e. 275 and make it mass, restart sequencing
-# NEXT : When last multi doesn't work, use previous.
-    elif len(multi_peaks_y)==1 and len(multi_peaks_b)==1:
-        mass_y=multi_peaks_y[0]
-        mass_b=multi_peaks_b[0]
-        seq.append(multi_AA[0])
-        print('seq : ',seq)#dev
-        continue
-    else:
-        mass_y=multi_peaks[0]
-        mass_b=multi_peaks_b[0]
-        seq.append(multi_AA[0])
-        print('seq : ',seq)#dev
-        continue
-    break
+            #print('in peaks_list')#dev
+            readings[mass_y][1].append(mass_y+x)
+            readings[mass_y][2].append(mass_b-x)
+            readings[mass_y][3].append(MW_AA[x])
+            pass
+        pass
+    #print('#=====#\nreadings :',readings,'\nx :',x,'\n#=====#')#dev
+    if x==186:# so that all possible peaks have been checked
+        multiple[0].insert(0,mass_y)
+        multiple[1].insert(0,len(readings[mass_y][3]))
+        #print('multiple',multiple,'\n#=====#')
+        if multiple[1][0]>=1:
+            #print('mass_y 1 :',mass_y,'mass_b 1 :',mass_b,'seq :',seq)
+            mass_b=readings[mass_y][2][0]
+            seq.append(readings[mass_y][3][0])
+            mass_y=readings[mass_y][1][0]
+            #print('mass_y 2 :',mass_y,'mass_b 2 :',mass_b,'seq :',seq)
+            pass
+        elif multiple[1][0]==0:
+            #print('multiple',multiple)
+            #del multiple[1][0]
+            #print('multiple',multiple)
+            print('readings is empty, trying from last branch')
+            #find last branch, with multiple[1]!=1
+            branch=next((i for i, x in enumerate(multiple[1]) if x>1), None)
+            #print('branch',branch)
+            #delete the multiple[0] and multiple [1] after the branch
+            del multiple[0][:branch]
+            del multiple[1][:branch]
+            #print('multiple',multiple)
+            #reset mass_y to before branch
+            mass_y=multiple[0][0]
+            #print('mass_y',mass_y)
+            #remove one branch
+            multiple[1][0]=multiple[1][0]-1
+            #delete the part after the branch in readings
+            for n in sorted(readings.keys()):
+                if n>mass_y:
+                    del readings[n]
+                    pass
+                pass
+            #print('multiple',multiple)
+            #delete the first reading after the branch
+            print(readings)
+            del readings[mass_y][1][0]
+            del readings[mass_y][2][0]
+            del readings[mass_y][3][0]
+            #reset seq to what it was before the branch
+            del seq[(len(seq)-branch):]
+            #use values of second branch
+            mass_b=readings[mass_y][2][0]
+            #print('mass_b',mass_b)
+            seq.append(readings[mass_y][3][0])
+            #print('seq',seq)
+            mass_y=readings[mass_y][1][0]
+            #print('mass_y',mass_y)
+            pass
+        pass
 
 ####    CALCULATING M/Z OF IONS
+#print('seq :',seq)
 for n in range(len(seq)-1):
-    mass_y_list.append(mass_y_list[n]+AA_MW[seq[n+1]])
-    mass_b_list.append(mass_b_list[n]-AA_MW[seq[n+1]])
-    #if mass_y_list[n] in peaks_list:
-    peaks_list.remove(mass_y_list[n])
-    #if  mass_b_list[n] in peaks_list:
-    peaks_list.remove(mass_b_list[n])
-print('peaks left :',peaks_list)
-mass_a_list=[]
-for n in range(len(seq)-1):
-    mass_a_list.append('')
-    if mass_b_list[n]-28 in peaks_list:
-        mass_a_list[n]=mass_b_list[n]-28
-    if  mass_a_list[n]!='':
-        peaks_list.remove(mass_a_list[n])
-print('final peaks :',peaks_list)
-
-print('m y :',mass_y_list,'\nm b :',mass_b_list,'\nm a :',mass_a_list)#dev
-
-
+# m/z of y" ions
+    masses_dict['y'].append(masses_dict['y'][n]+AA_MW[seq[n+1]])
+    peaks_list.remove(masses_dict['y'][-1])
+# m/z of b ions
+    masses_dict['b'].append(masses_dict['b'][n]-AA_MW[seq[n+1]])
+    peaks_list.remove(masses_dict['b'][-1])
+    pass
+#print(masses_dict['y'],masses_dict['b'])#dev
+#print('peaks_list :',peaks_list)#dev
+#print('multiple',multiple)#dev
+masses_dict['b'].remove(1)
+masses_dict['b'].insert(0,mol)
+# m/z of a ions
+finding_ions(peaks_list,seq,masses_dict['b'],'a',-28)
+# NEXT : thes have to be present in the ion not the whole seq
+# m/z of ions with H2O loss
+if any(STED in residue for residue in seq for STED in 'STED'):
+    finding_ions(peaks_list,seq,masses_dict['y'],'y-H2O',-18)
+    finding_ions(peaks_list,seq,masses_dict['b'],'b-H2O',-18)
+    finding_ions(peaks_list,seq,masses_dict['a'],'a-H2O',-18)
+    pass
+# m/z of ions with NH3 loss
+if any(RKNQ in residue for residue in seq for RKNQ in 'RKNQ'):
+    finding_ions(peaks_list,seq,masses_dict['y'],'y-NH3',-17)
+    finding_ions(peaks_list,seq,masses_dict['b'],'b-NH3',-17)
+    finding_ions(peaks_list,seq,masses_dict['a'],'a-NH3',-17)
+    pass
 ####    OUTPUT
-print('list_multi : ',list_multi)#dev
+# NEXT : do not display lists that are empty
+#if all(0==value for value in masses_dict['y-NH3']):
+#    print('y-NH3 is empty')
+
+#print('final peaks :',peaks_list)#dev
+#print('seq',seq)#dev
+#print(readings)#dev
+print('\n{0:<3}{1:<5}{2:<7}{3:<7}{4:<7}{5:<7}{6:<7}{7:<7}{8:<7}{9:<7}{10:<7}'
+    .format('n','Seq','y','y-H2O','y-NH3','b','b-H2O','b-NH3','a','a-H2O','a-NH3'))
+print('-----------------------------------------------------------------------')
+for n in range(len(seq)):
+    print('{0:<3}{1:<5}{2:<7}{3:<7}{4:<7}{5:<7}{6:<7}{7:<7}{8:<7}{9:<7}{10:<7}'
+    .format((len(seq)-n),seq[n],masses_dict['y'][n],masses_dict['y-H2O'][n],
+    masses_dict['y-NH3'][n],masses_dict['b'][n],masses_dict['b-H2O'][n],
+    masses_dict['b-NH3'][n],masses_dict['a'][n],masses_dict['a-H2O'][n],
+    masses_dict['a-NH3'][n]))
+
+#print('list_multi : ',list_multi)#dev
 print('\nThe sequence is',len(seq),'residues long.')
 
 ####    END
 #Timing is the coolest thing !
 end=timer()
-print ('\nDone in {0} secs.'.format(end-start))
+print ('\nDone in {0} secs.'.format(end-start),'\n')
